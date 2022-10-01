9fc466d22e00a8622b673026b1448a660c78973a
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-10-01 12:13:18 -0300

    my_store/viewa.py and template/contact/view.html has modified

--- my_store/views.py
+++ my_store/views.py
@@ -1,5 +1,7 @@
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def home_page(request):
@@ -19,10 +21,22 @@ def about_page(request):
 
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Página de contato",
-        "content": "Bem vindo a página de contato"
-    }
+        "content": "Bem vindo a página de contato",
+        "form": contact_form
+    }  
+
+    if contact_form.is_valid():
+      print(contact_form.cleaned_data['nome_completo'])
+
     if request.method == "POST":
+      #exibir todos os dados que chega no POST do formulário
         print(request.POST)
+        #exibir dado específico que chega no POST do formulário com o .get('valor')
+        print(request.POST.get('nome_completo'))
+        print(request.POST.get('email'))
+        print(request.POST.get('mensagem'))
     return render(request, 'contact/view.html', context)
+
