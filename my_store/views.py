0f2bae917b2b6352fffe91e877778a890ec2a655
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-10-02 16:17:13 -0300

    modify my_store/forms.py, create template/auth/login.html and template/auth/register.html

--- my_store/views.py
+++ my_store/views.py
@@ -1,42 +1,80 @@
-from django import forms
+
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
-from .forms import ContactForm
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm, LoginForm, RegisterForm
 
 
 def home_page(request):
     context = {
-        "title": "Pagina principal",
-        "content": "Bem vindo a página principal"
+        "title": "Home Page",
+        "content": "Bem vindo a Home Page",
     }
-    return render(request, 'home_page.html', context)
+    if request.user.is_authenticated:
+        context["premium_content"] = "Você é um usuário Premium"
+    return render(request, "home_page.html", context)
 
 
 def about_page(request):
     context = {
-        "title": "Pagina sobre",
-        "content": "Bem vindo a página sobre"
+        "title": "About Page",
+        "content": "Bem vindo a About Page"
     }
-    return render(request, 'about/view.html', context)
+    return render(request, "about/view.html", context)
 
 
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
     context = {
-        "title": "Página de contato",
-        "content": "Bem vindo a página de contato",
+        "title": "Contact Page",
+        "content": "Bem vindo a Contact Page",
         "form": contact_form
-    }  
-
+    }
     if contact_form.is_valid():
-      print(contact_form.cleaned_data['nome_completo'])
+        print(contact_form.cleaned_data)
+    return render(request, "contact/view.html", context)
 
-    if request.method == "POST":
-      #exibir todos os dados que chega no POST do formulário
-        print(request.POST)
-        #exibir dado específico que chega no POST do formulário com o .get('valor')
-        print(request.POST.get('nome_completo'))
-        print(request.POST.get('email'))
-        print(request.POST.get('mensagem'))
-    return render(request, 'contact/view.html', context)
 
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print("User logged in")
+    # print(request.user.is_authenticated)
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        # print(request.user.is_authenticated)
+        if user is not None:
+            # print(request.user.is_authenticated)
+            login(request, user)
+            print("Login válido")
+            # Redireciona para uma página de sucesso.
+            return redirect("/")
+        else:
+            # Retorna uma mensagem de erro de 'invalid login'.
+            print("Login inválido")
+    return render(request, "auth/login.html", context)
+
+
+User = get_user_model()
+
+
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, "auth/register.html", context)
