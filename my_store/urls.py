70826cb6a524ab3155990dcd7cadaca1dba24107
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-10-03 21:46:36 -0300

    insert STATIFILES_DIRS in src/my_store/settings.py

--- my_store/urls.py
+++ my_store/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 from .views import home_page, about_page, contact_page, login_page, register_page
@@ -11,3 +14,6 @@ urlpatterns = [
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
